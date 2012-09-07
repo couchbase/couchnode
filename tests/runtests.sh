74ebe939939178d3e94a453df8596c8a7d636848
Trond Norbye <trond.norbye@gmail.com>
2012-09-07 11:31:02 +0200

    Use exit code 64 as a special value
    We need a way to signal out that we're skipping a test ;)
    
    Change-Id: Ie8560e762b15487fcedb0d751c260b5484557cb1
    Reviewed-on: http://review.couchbase.org/20615
    Reviewed-by: Mordechai Nunberg <mnunberg@haskalah.org>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -2,5 +2,15 @@
 for t in $@; do
     node $t
     rv=$?
-    test $rv -eq 0 && echo "$t .. OK" || echo "$t .. FAIL"
+    case $rv in
+      0)
+         echo "$t .. OK"
+         ;;
+      64)
+         echo "$t .. SKIP"
+         ;;
+      *)
+        echo "$t .. FAIL"
+         ;;
+    esac
 done
