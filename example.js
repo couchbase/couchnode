aa5174750c634910f4c58f1d0822fb1daefb866b
Chris Anderson <jchris@apache.org>
2012-11-01 08:34:07 -0700

    made the example more realistic with better logging
    Change-Id: Iec174775f531f4a0f84d87966d21a17e28cf9b12
    Reviewed-on: http://review.couchbase.org/22203
    Reviewed-by: Chris Anderson <jchris@couchbase.com>
    Tested-by: Chris Anderson <jchris@couchbase.com>

--- example.js
+++ example.js
@@ -1,24 +1,23 @@
 var couchbase = require(__dirname),
     http = require("http");
 
-couchbase.connect({
-        "username" : "Administrator",
-        "password":"animal",
-        "hostname" : "localhost:8091",
-        "bucket":"default"
-    }, function(err, bucket) {
-        if (err) {throw(err)}
+var port = 8080;
 
-        http.createServer(function(req, resp) {
-            bucket.get("hitcount", function(err, doc, meta) {
-                if (!doc) {
-                    doc = {count:0};
-                }
-                doc.count++;
-                resp.writeHead(200);
-                bucket.set("hitcount", doc, meta, function(err) {
-                    resp.end('<p>The server has seen '+doc.count+' hits</p>');
-                })
+couchbase.connect(require(__dirname+'/tests/config.json'), function(err, bucket) {
+    if (err) {throw(err)}
+
+    http.createServer(function(req, resp) {
+        bucket.get("hitcount", function(err, doc, meta) {
+            if (!doc) {
+                doc = {count:0};
+            }
+            doc.count++;
+            resp.writeHead(200);
+            console.log("hits", doc.count);
+            bucket.set("hitcount", doc, meta, function(err) {
+                resp.end('<p>The server has seen '+doc.count+' hits</p>');
             })
-        }).listen(8080);
+        })
+    }).listen(port);
+    console.log("listening on http://localhost:" + port);
 })
