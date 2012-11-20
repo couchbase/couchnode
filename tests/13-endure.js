4cca88207681e08a8fe05970481149257dc0b212
Brett Lawson <brett19@gmail.com>
2012-11-20 07:52:52 -0400

    Implemented ENDURE (user-friendly version of OBSERVE)
    Change-Id: I6d71deec127ee07114bbcfa4124b9d10072d7dca
    Reviewed-on: http://review.couchbase.org/22739
    Tested-by: Brett Lawson <brett19@gmail.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

---
+++ tests/13-endure.js
@@ -0,0 +1,28 @@
+var setup = require('./setup'),
+    assert = require('assert');
+
+setup(function(err, cb) {
+    assert(!err, "setup failure");
+
+    cb.on("error", function (message) {
+        console.log("ERROR: [" + message + "]");
+        process.exit(1);
+    });
+
+    var testkey = "13-endure.js"
+
+    cb.set(testkey, "bar", function(err,meta) {
+        assert(!err, "Failed to store object");
+
+        var endureOpts = {
+            persisted: 1,
+            replicated: 0
+        };
+        cb.endure(testkey, endureOpts, function(err, meta) {
+            assert(!err, "Failed to complete endure");
+            assert(meta);
+
+            process.exit(0);
+        });
+    });
+});
