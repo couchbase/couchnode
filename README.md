68ee2fb493702a0d16556e6026f81c1023694d3f
Trond Norbye <trond.norbye@gmail.com>
2013-01-18 15:53:19 +0300

    Doc fixes
    Change-Id: If40e0a8914df281ddaad98e44538760d3903fd70
    Reviewed-on: http://review.couchbase.org/24057
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- README.md
+++ README.md
@@ -6,15 +6,16 @@ This library allows you to connect to a Couchbase cluster from node.js
 Basic installation and usage
 --------------------
 
-To install this module, we'll assume you are using [NPM](https://npmjs.org).
-However it is not as simple as a regular JavaScript module,
-as it depends on the C-library for Couchbase clients,
-[libcouchbase](https://github.com/couchbase/libcouchbase). Libcouchbase
-also powers other dynamic language clients, such as Ruby and PHP, so
-if you've worked with those clients you should feel right at home.
+To install this module, we'll assume you are using
+[NPM](https://npmjs.org).  However it is not as simple as a regular
+JavaScript module, as it depends on the C-library for Couchbase
+clients, [libcouchbase](https://github.com/couchbase/libcouchbase).
+Libcouchbase also powers other dynamic language clients, such as Ruby
+and PHP, so if you've worked with those clients you should feel right
+at home.
 
-First step is to install libcouchbase (the 2.0 version). On a Mac
-with homebrew this should be as easy as running:
+First step is to install libcouchbase (the 2.0 version). On a Mac with
+homebrew this should be as easy as running:
 
     brew install libcouchbase
 
@@ -43,7 +44,6 @@ details.](https://github.com/couchbase/couchnode/tree/master/tests)
     replace:   cb.replace(testkey, "bar", function(err, meta) {})
     delete:    cb.delete(testkey, function (err, meta) {})
     multiget:  cb.get(['key1', 'key2', '...'], function(err, doc, meta) {})
-
 
 Contributing changes
 --------------------
--- example.js
+++ example.js
@@ -1,21 +1,45 @@
-var couchbase = require("couchbase"),
-    http = require("http");
+var couchbase = require("couchbase");
+var http = require("http");
+var fs = require("fs");
+var util = require("util");
 
 var port = 8080;
 
-couchbase.connect(require(__dirname+'/tests/config.json'), function(err, bucket) {
-    if (err) {throw(err)}
+var configFilename = 'config.json';
+if (fs.existsSync(configFilename)) {
+    config = JSON.parse(fs.readFileSync(configFilename));
+} else {
+    console.log(configFilename + " not found. Using default");
+    config = { };
+}
+
+couchbase.connect(config, function(err, bucket) {
+    if (err) {
+        // For some reason we failed to make a connection to the
+        // Couchbase cluster.
+        throw err;
+    }
 
     http.createServer(function(req, resp) {
         bucket.get("hitcount", function(err, doc, meta) {
+            // @todo check the error reason!
             if (!doc) {
                 doc = {count:0};
             }
+
             doc.count++;
             resp.writeHead(200);
             console.log("hits", doc.count);
             bucket.set("hitcount", doc, meta, function(err) {
-                resp.end('<p>The server has seen '+doc.count+' hits</p>');
+                if (err) {
+                    console.warn("Failed to store hit counter: " +
+                                 util.inspect(err));
+                    resp.end("<p>Internal server error. " +
+                             "Failed to store:</p><pre>" + util.inspect(err) +
+                             "</pre>");
+                } else {
+                    resp.end('<p>The server has seen '+doc.count+' hits</p>');
+                }
             })
         })
     }).listen(port);
