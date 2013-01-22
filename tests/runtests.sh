aac9c3574fbae754713332fb8ce2dd0e70f4f86f
Kevin Reilly <kevin.reilly@touchfactor.com>
2013-01-22 12:57:26 -0800

    Improved runtests.sh and removed bug.js
    runtests.sh now has counts and colors
    Test case provided by bug.js appears to be resolved
    
    Change-Id: I31ae109e938219d40207e0fe7abedaddbe1ad3ca
    Reviewed-on: http://review.couchbase.org/24140
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,16 +1,45 @@
 #!/bin/sh
-for t in $@; do
-    node $t
-    rv=$?
-    case $rv in
-      0)
-         echo "$t .. OK"
-         ;;
-      64)
-         echo "$t .. SKIP"
-         ;;
-      *)
-        echo "$t .. FAIL"
-         ;;
-    esac
+echo "\033[1;33mRunning tests...\033[m";
+
+# Set count defaults
+runs=0
+passed=0
+skipped=0
+failed=0
+
+# Iterate over all .js files in this directory
+for t in *.js; do
+    if [ $t != "setup.js" ]
+      then
+        node $t
+        rv=$?
+        case $rv in
+          0)
+             passed=`expr $passed + 1`;
+             echo "$t .. \033[1;32mOK\033[m"
+             ;;
+          64)
+             skipped=`expr $skipped + 1`;
+             echo "$t .. \033[1;33mSKIP\033[m"
+             ;;
+          *)
+             failed=`expr $failed + 1`;
+             echo "$t .. \033[1;31mFAIL\033[m"
+             ;;
+        esac
+        runs=`expr $runs + 1`
+    fi
 done
+
+# Output the results
+echo "\033[1;32m$passed\033[m \033[1;33mout of\033[m \033[1;32m$runs\033[m \033[1;33mtests passed\033[m";
+
+if [ $skipped -gt 0 ]
+then
+    echo "\033[1;33m$skipped tests skipped\033[m"
+fi
+
+if [ $failed -gt 0 ]
+then
+    echo "\033[1;31m$failed tests failed\033[m"
+fi
