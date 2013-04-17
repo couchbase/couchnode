4cee23391e180810c50640469a7f5f5b85ccaafc
Trond Norbye <trond.norbye@gmail.com>
2013-04-18 00:07:02 +0200

    JSCBC-16: Add unit test for bulk load
    Change-Id: I04ffe125204b6d554e4236f22bec3aea093a0a16
    Reviewed-on: http://review.couchbase.org/25739
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

---
+++ tests/16-JSCBC-16.js
@@ -0,0 +1,25 @@
+var setup = require('./setup'),
+    assert = require('assert');
+
+var max=6000;
+var current=0;
+
+setup(function(err, cb) {
+    assert(!err, "setup failure");
+
+    cb.on("error", function (message) {
+        console.log("ERROR: [" + message + "]");
+        process.exit(1);
+    });
+
+
+    for (var i=0; i < max; ++i){
+	cb.set("new_"+i, "something", function(err, meta) {
+            assert(!err, "Failed to store value");
+            ++current;
+            if (current == max) {
+               process.exit(1);
+            }
+	})
+    }
+});
