1fa3e3dc08b8cd0526af67c52c1199300deb9a27
Brett Lawson <brett19@gmail.com>
2013-05-23 15:11:28 -0300

    Fixed test runner to display slash codes.
    Change-Id: Ia032387eb120ad2004cd6d8deae111bdf4c111de
    Reviewed-on: http://review.couchbase.org/26500
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-echo "\033[1;33mRunning tests...\033[m";
+foo=`echo -e`
+if [ "$foo" = "" ]
+then
+  dashe=-e
+fi
+
+echo $dashe "\033[1;33mRunning tests...\033[m";
 
 # Set count defaults
 runs=0
@@ -16,15 +22,15 @@ for t in `ls *.js | sort`; do
         case $rv in
           0)
              passed=`expr $passed + 1`;
-             echo "$t .. \033[1;32mOK\033[m"
+             echo $dashe "$t .. \033[1;32mOK\033[m"
              ;;
           64)
              skipped=`expr $skipped + 1`;
-             echo "$t .. \033[1;33mSKIP\033[m"
+             echo $dashe "$t .. \033[1;33mSKIP\033[m"
              ;;
           *)
              failed=`expr $failed + 1`;
-             echo "$t .. \033[1;31mFAIL\033[m"
+             echo $dashe "$t .. \033[1;31mFAIL\033[m"
              ;;
         esac
         runs=`expr $runs + 1`
@@ -32,14 +38,14 @@ for t in `ls *.js | sort`; do
 done
 
 # Output the results
-echo "\033[1;32m$passed\033[m \033[1;33mout of\033[m \033[1;32m$runs\033[m \033[1;33mtests passed\033[m";
+echo $dashe "\033[1;32m$passed\033[m \033[1;33mout of\033[m \033[1;32m$runs\033[m \033[1;33mtests passed\033[m";
 
 if [ $skipped -gt 0 ]
 then
-    echo "\033[1;33m$skipped tests skipped\033[m"
+    echo $dashe "\033[1;33m$skipped tests skipped\033[m"
 fi
 
 if [ $failed -gt 0 ]
 then
-    echo "\033[1;31m$failed tests failed\033[m"
+    echo $dashe "\033[1;31m$failed tests failed\033[m"
 fi
