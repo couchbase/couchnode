5be4fb759e43d5dfe2b08f99751155f6f169bebe
Brett Lawson <brett19@gmail.com>
2017-05-30 01:54:57 -0700

    JSCBC-381: Fix bug causing exists method to return incorrect results.
    Change-Id: I02b9b65913926e0885f711b6364d09de62fb7468
    Reviewed-on: http://review.couchbase.org/78868
    Reviewed-by: Mark Nunberg <mark.nunberg@couchbase.com>
    Tested-by: Brett Lawson <brett19@gmail.com>

--- test/subdoc.test.js
+++ test/subdoc.test.js
@@ -6,6 +6,37 @@ var harness = require('./harness.js');
 var H = harness;
 
 describe('#subdoc', function () {
+  it('should get correctly', function(done) {
+    var itemMap = {test: 'lol'};
+
+    H.b.upsert('sdGet', itemMap, H.okCallback(function() {
+      H.b.lookupIn('sdGet')
+          .get('test')
+          .execute(H.okCallback(function(res) {
+            assert(res.content('test') === 'lol');
+            assert(res.contentByIndex(0) === 'lol');
+
+            done();
+          }));
+    }));
+  });
+
+  it('should exists correctly', function(done) {
+    var itemMap = {test: 'lol'};
+
+    H.b.upsert('sdExists', itemMap, H.okCallback(function() {
+      H.b.lookupIn('sdExists')
+          .exists('test')
+          .exists('nope')
+          .execute(function(err, res) {
+            assert(res.exists('test') === true);
+            assert(res.exists('nope') === false);
+
+            done();
+          });
+    }));
+  });
+
   it('should getCount correctly', function(done) {
     var itemMap = {test: [1, 2, 3, 4, 5]};
 
