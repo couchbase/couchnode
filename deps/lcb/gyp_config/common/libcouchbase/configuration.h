fcc70600a4e4b2fa438d86f91f2599fec4137892
Brett Lawson <brett19@gmail.com>
2018-02-08 02:02:57 -0800

    Correct error in libcouchbase release meta-data.
    Change-Id: I18f756b5f4f06295c481edc3cb00edd1ebc2c8fd
    Reviewed-on: http://review.couchbase.org/89072
    Reviewed-by: Brett Lawson <brett19@gmail.com>
    Tested-by: Brett Lawson <brett19@gmail.com>

--- deps/lcb/gyp_config/common/libcouchbase/configuration.h
+++ deps/lcb/gyp_config/common/libcouchbase/configuration.h
@@ -33,7 +33,7 @@
  */
 
 /** @brief libcouchbase version string */
-#define LCB_VERSION_STRING "2.8.3-njs"
+#define LCB_VERSION_STRING "2.8.4-njs"
 
 /**@brief libcouchbase hex version
  *
@@ -50,12 +50,12 @@
  * 2.1.3	| 0x020103
  * 3.0.15	| 0x030015
  */
-#define LCB_VERSION 0x020803
+#define LCB_VERSION 0x020804
 
 /**@brief The SCM revision ID
  * @see LCB_CNTL_CHANGESET
  */
-#define LCB_VERSION_CHANGESET "8ce52f9294cfef59b9ff44f3cfc18cc3ca8c2860"
+#define LCB_VERSION_CHANGESET "c701796502dcabc846da5992964d828c426b264f"
 
 /**@brief The client ID
  */
