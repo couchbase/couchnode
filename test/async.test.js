04b6acff561e723adf7aebc0940064f4e74128a6
Brett Lawson <brett19@gmail.com>
2019-02-05 11:51:03 -0800

    JSCBC-533: Added async tracking for connection.
    Motivation
    ----------
    Previously, performing operations before the client was
    connected would cause async state to be lost.  This would
    cause issues with any tools that rely on the asynchronous
    stack tracking used by Node.js
    
    Changes
    -------
    Added AsyncResource's for pending operations when they
    are queued for later execution.
    
    Change-Id: I2adcf76fa531d80c7ecc2017b6faa98e223b66af
    Reviewed-on: http://review.couchbase.org/104541
    Reviewed-by: Matt Ingenthron <ingenthr@gmail.com>
    Tested-by: Brett Lawson <brett19@gmail.com>

---
+++ test/async.test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var assert = require('assert');
+var cls_hooked = require('cls-hooked');
+var async_hooks = require('../lib/async_hooks_stub');
+var harness = require('./harness.js');
+
+var asyncns = cls_hooked.createNamespace('test');
+
+describe('#async', function() {
+  function allTests(H) {
+    if (async_hooks.supported) {
+      it('should properly handle async contexts', function(done) {
+        asyncns.run(function(ctx) {
+          asyncns.set('value', 'ctxisworking');
+
+          H.b.get('test', function(err, res) {
+            assert.equal(asyncns.get('value'),
+              'ctxisworking');
+
+            H.b.get('test', function(err, res) {
+              assert.equal(asyncns.get('value'),
+                'ctxisworking');
+              done();
+            });
+          });
+        });
+      });
+    } else {
+      it.skip('should properly handle async contexts');
+    }
+  }
+
+  describe('#RealBucket', allTests.bind(this, harness));
+  describe('#MockBucket', allTests.bind(this, harness.mock));
+});
