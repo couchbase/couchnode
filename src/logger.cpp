849f5e3ff8733d11fd542f66db8b0acbcf83f2e2
Brett Lawson <brett19@gmail.com>
2020-12-01 10:19:17 -0800

    JSCBC-815: Avoid re-using consumed va_list.
    Motivation
    ----------
    Due to the fact that using a va_list fundamentally alters it and
    makes it no longer valid, we need to make a copy of the list in
    order to enable us to call sprintf twice in the case that our
    buffer needs to be resized.
    
    Changes
    -------
    Modified logger to copy the va_list we receive before we invoke
    the first sprintf call such that we still have a valid copy of
    the va_list in case we need to invoke sprintf again.
    
    Change-Id: I9ec650c934f3db49445d6d2d09facc0140bf2a9b
    Reviewed-on: http://review.couchbase.org/c/couchnode/+/141256
    Reviewed-by: Matt Ingenthron <ingenthr@gmail.com>
    Reviewed-by: Charles Dixon <chvckd@gmail.com>
    Tested-by: Brett Lawson <brett19@gmail.com>

--- src/logger.cpp
+++ src/logger.cpp
@@ -29,19 +29,28 @@ void Logger::handler(unsigned int iid, const char *subsys, int severity,
                      va_list ap)
 {
     Nan::HandleScope scope;
+    va_list apCopy;
 
     if (!_logBuffer) {
         _logBufferLen = 2048;
         _logBuffer = new char[_logBufferLen];
     }
 
-    int genLen = vsnprintf(_logBuffer, _logBufferLen, fmt, ap);
+    // Due the fact that the call to vsnprintf modifies the va_list itself, we
+    // cannot invoke vsnprintf twice, in order to get around this, we copy this
+    // list for the first call and then use the original list if needed for the
+    // second call.
+    va_copy(apCopy, ap);
+
+    int genLen = vsnprintf(_logBuffer, _logBufferLen, fmt, apCopy);
     if (genLen >= _logBufferLen) {
         _logBufferLen = genLen + 1;
         delete[] _logBuffer;
         _logBuffer = new char[_logBufferLen];
         vsnprintf(_logBuffer, _logBufferLen, fmt, ap);
     }
+
+    va_end(apCopy);
 
     Local<Object> infoObj = Nan::New<Object>();
     Nan::Set(infoObj, Nan::New<String>("severity").ToLocalChecked(),
