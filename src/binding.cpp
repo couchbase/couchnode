fb68d41b0855acc98f3d6ea3490e5f32f6175931
Jared Casey <jared.casey@couchbase.com>
2024-10-14 11:20:46 -0600

    JSCBC-1280: Enable use of C++ file logger
    Changes
    =======
    * If CBPPLOGFILE environment variable is set, use C++ core file logger
      instead of console logger.  NOTE:  CBPPLOGLEVEL must be set as well.
    
    Change-Id: I6ecb0c3b489f187a58b902ae39c634c96b617605
    Reviewed-on: https://review.couchbase.org/c/couchnode/+/217899
    Reviewed-by: Mateusz <matt.wozakowski@couchbase.com>
    Tested-by: Jared Casey <jared.casey@couchbase.com>

--- src/binding.cpp
+++ src/binding.cpp
@@ -9,7 +9,6 @@
 #include <core/logger/configuration.hxx>
 #include <core/meta/version.hxx>
 #include <napi.h>
-#include <spdlog/spdlog.h>
 
 namespace couchnode
 {
@@ -39,40 +38,41 @@ Napi::Value shutdown_logger(const Napi::CallbackInfo &info)
 
 Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
-    spdlog::set_pattern("[%Y-%m-%d %T.%e] [%P,%t] [%^%l%$] %oms, %v");
-
-    auto spdLogLevel = spdlog::level::off;
     auto cbppLogLevel = couchbase::core::logger::level::off;
     {
         const char *logLevelCstr = getenv("CBPPLOGLEVEL");
         if (logLevelCstr) {
             std::string logLevelStr = logLevelCstr;
             if (logLevelStr == "trace") {
-                spdLogLevel = spdlog::level::trace;
                 cbppLogLevel = couchbase::core::logger::level::trace;
             } else if (logLevelStr == "debug") {
-                spdLogLevel = spdlog::level::debug;
                 cbppLogLevel = couchbase::core::logger::level::debug;
             } else if (logLevelStr == "info") {
-                spdLogLevel = spdlog::level::info;
                 cbppLogLevel = couchbase::core::logger::level::info;
             } else if (logLevelStr == "warn") {
-                spdLogLevel = spdlog::level::warn;
                 cbppLogLevel = couchbase::core::logger::level::warn;
             } else if (logLevelStr == "err") {
-                spdLogLevel = spdlog::level::err;
                 cbppLogLevel = couchbase::core::logger::level::err;
             } else if (logLevelStr == "critical") {
-                spdLogLevel = spdlog::level::critical;
                 cbppLogLevel = couchbase::core::logger::level::critical;
             }
         }
     }
+
     if (cbppLogLevel != couchbase::core::logger::level::off) {
-        couchbase::core::logger::create_console_logger();
+        const char *logFileCstr = getenv("CBPPLOGFILE");
+        if (logFileCstr) {
+            std::string logFileStr = logFileCstr;
+            couchbase::core::logger::configuration configuration{};
+            configuration.filename = logFileStr;
+            configuration.log_level = cbppLogLevel;
+            couchbase::core::logger::create_file_logger(configuration);
+
+        } else {
+            couchbase::core::logger::create_console_logger();
+            couchbase::core::logger::set_log_levels(cbppLogLevel);
+        }
     }
-    spdlog::set_level(spdLogLevel);
-    couchbase::core::logger::set_log_levels(cbppLogLevel);
 
     AddonData::Init(env, exports);
     Constants::Init(env, exports);
